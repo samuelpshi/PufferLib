7f8da4941a7da4695159397b7ca07fc869946b46
sam <samuelshi007@gmail.com>
2026-06-23 16:17:22 -0400

    Add flappy_basic env: tutorial 2-cell vertical environment

---
+++ ocean/flappy_basic/binding.c
@@ -0,0 +1,19 @@
+#include "flappy.h"
+#define OBS_SIZE 1
+#define NUM_ATNS 1
+#define ACT_SIZES {2}
+#define OBS_TENSOR_T FloatTensor
+
+#define Env Flappy
+#include "vecenv.h"
+
+void my_init(Env* env, Dict* kwargs) {
+    env->num_agents = 1;
+}
+
+void my_log(Log* log, Dict* out) {
+    dict_set(out, "perf", log->perf);
+    dict_set(out, "score", log->score);
+    dict_set(out, "episode_return", log->episode_return);
+    dict_set(out, "episode_length", log->episode_length);
+}
