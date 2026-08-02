75d47c15e5f2036e266fd6425cdc54915f93d5f5
Sam Shi <samuelshi007@gmail.com>
2026-08-02 19:08:15 -0400

    flappy: add puffernet demo with trained weights

--- ocean/flappy/flappy.c
+++ ocean/flappy/flappy.c
@@ -1,13 +1,6 @@
-/* Pure C demo file for Flappy. Build it with:
- * bash scripts/build_ocean.sh target local (debug)
- * bash scripts/build_ocean.sh target fast
- * We suggest building and debugging your env in pure C first. You
- * get faster builds and better error messages. To keep this example
- * simple, it does not include C neural nets. See Target for that.
- */
-
 #include <stdlib.h>
 #include "flappy.h"
+#include "puffernet.h"
 
 void demo() {
     Flappy env = {.rng = 42};
@@ -16,61 +9,25 @@ void demo() {
     env.rewards = (float*)calloc(1, sizeof(float));
     env.terminals = (float*)calloc(1, sizeof(float));
 
+    Weights* weights = load_weights("resources/flappy/flappy_weights.bin");
+    int logit_sizes[1] = {2};
+    PufferNet* net = make_puffernet(weights, 1, 6, 128, 1, logit_sizes, 1);
+
     c_reset(&env);
     c_render(&env);
     while (!WindowShouldClose()) {
         
-        env.actions[0] = 0.0f;
-        if (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W)) env.actions[0] = FLAP;
-
-        c_step(&env);
-        c_render(&env);
-    }
-
-    free(env.observations);
-    free(env.actions);
-    free(env.rewards);
-    free(env.terminals);
-    c_close(&env);
-}
-
-int main() {
-    demo();
-    return 0;
-}
-
-
-
- /*#include "flappy.h"
-#include "puffernet.h"
-
-void demo() {
-    Flappy env = {.rng = 42};
-    env.observations = (float*)calloc(1, sizeof(float));
-    env.actions = (float*)calloc(1, sizeof(float));
-    env.rewards = (float*)calloc(1, sizeof(float));
-    env.terminals = (float*)calloc(1, sizeof(float));
-
-    Weights* weights = load_weights("resources/flappy/flappy_weights.bin");
-    int logit_sizes[1] = {2};
-    PufferNet* net = make_puffernet(weights, 1, 1, 128, 1, logit_sizes, 1);
-
-    c_reset(&env);
-    c_render(&env);
-    while (!WindowShouldClose()) {
         if (IsKeyDown(KEY_LEFT_SHIFT)) {
             env.actions[0] = 0.0f;
-            if (IsKeyDown(KEY_UP)    || IsKeyDown(KEY_W)) env.actions[0] = UP;
-            if (IsKeyDown(KEY_DOWN)  || IsKeyDown(KEY_S)) env.actions[0] = DOWN;
+            if (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W)) env.actions[0] = FLAP;
         } else {
-            float obs_f[1];
-            for(int i=0; i<1; i++) obs_f[i] = (float)env.observations[i];
-            forward_puffernet(net, obs_f, env.actions);
+            forward_puffernet(net, env.observations, env.actions);
         }
+
         c_step(&env);
         c_render(&env);
     }
-    
+
     free_puffernet(net);
     free(weights);
 
@@ -85,5 +42,3 @@ int main() {
     demo();
     return 0;
 }
-
-*/
